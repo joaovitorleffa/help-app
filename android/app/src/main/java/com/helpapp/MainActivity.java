e6093659ababb7d54756fb505e3f6276f7656c6b
joaovitor <joaovitor@joaovitors-Mac-Pro.local>
2021-11-15 19:33:11 -0300

    :arrow_up: upgrage expo

--- android/app/src/main/java/com/helpapp/MainActivity.java
+++ android/app/src/main/java/com/helpapp/MainActivity.java
@@ -6,17 +6,17 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
-import expo.modules.splashscreen.singletons.SplashScreen;
-import expo.modules.splashscreen.SplashScreenImageResizeMode;
-import android.os.Bundle;
+
+import expo.modules.ReactActivityDelegateWrapper;
 
 public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    // Set the theme to AppTheme BEFORE onCreate to support
+    // coloring the background, status bar, and navigation bar.
+    // This is required for expo-splash-screen.
+    setTheme(R.style.AppTheme);
     super.onCreate(null);
-    // SplashScreen.show(...) has to be called after super.onCreate(...)
-    // Below line is handled by '@expo/configure-splash-screen' command and it's discouraged to modify it manually
-    SplashScreen.show(this, SplashScreenImageResizeMode.CONTAIN, ReactRootView.class, false);
   }
 
 
@@ -31,16 +31,14 @@ public class MainActivity extends ReactActivity {
 
     @Override
     protected ReactActivityDelegate createReactActivityDelegate() {
-        return new ReactActivityDelegate(this, getMainComponentName()) {
-            @Override
-            protected ReactRootView createRootView() {
-                return new RNGestureHandlerEnabledRootView(MainActivity.this);
-            }
-        };
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
+        return new ReactActivityDelegateWrapper(
+           this,
+           new ReactActivityDelegate(this, getMainComponentName()) {
+               @Override
+               protected ReactRootView createRootView() {
+                   return new RNGestureHandlerEnabledRootView(MainActivity.this);
+               }
+           }
+       );
     }
 }
--- src/components/templates/Organization/ProfileInfo/index.tsx
+++ src/components/templates/Organization/ProfileInfo/index.tsx
@@ -4,7 +4,6 @@ import { ProfileBanner } from '@organisms/Organization/ProfileBanner';
 import { ProfileDescription } from '@organisms/Organization/ProfileDescription';
 
 import { Container, Wrapper } from './styles';
-import SkeletonContent from 'react-native-skeleton-content';
 import { useRem } from 'responsive-native';
 import { useTheme } from 'styled-components';
 
@@ -30,61 +29,16 @@ export function ProfileInfo({
 
   return (
     <Container>
-      <SkeletonContent
-        isLoading={isLoading}
-        containerStyle={{ width: '100%' }}
-        layout={[{ key: '1', width: '100%', height: rem(14) }]}>
-        <ProfileBanner uri={uri} />
-      </SkeletonContent>
-      <SkeletonContent
-        isLoading={isLoading}
-        containerStyle={{ width: '100%' }}
-        layout={[
-          {
-            key: '1',
-            width: '40%',
-            height: rem(theme.fonts.size.xl),
-            marginTop: 24,
-            marginHorizontal: theme.spacing.grid,
-          },
-          {
-            key: '2',
-            width: '60%',
-            height: rem(theme.fonts.size.lg),
-            marginTop: 12,
-            marginHorizontal: theme.spacing.grid,
-          },
-          {
-            key: '3',
-            width: '60%',
-            height: rem(theme.fonts.size.md),
-            marginTop: 24,
-            marginHorizontal: theme.spacing.grid,
-          },
-          {
-            key: '4',
-            width: '70%',
-            height: rem(theme.fonts.size.md),
-            marginTop: 6,
-            marginHorizontal: theme.spacing.grid,
-          },
-          {
-            key: '5',
-            width: '80%',
-            height: rem(theme.fonts.size.md),
-            marginTop: 6,
-            marginHorizontal: theme.spacing.grid,
-          },
-        ]}>
-        <Wrapper>
-          <ProfileDescription
-            name={name}
-            email={email}
-            description={description}
-            onLogout={onLogout}
-          />
-        </Wrapper>
-      </SkeletonContent>
+      <ProfileBanner uri={uri} />
+
+      <Wrapper>
+        <ProfileDescription
+          name={name}
+          email={email}
+          description={description}
+          onLogout={onLogout}
+        />
+      </Wrapper>
     </Container>
   );
 }
