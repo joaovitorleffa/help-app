c6c55f8b81975828379db0d9d3bc023b08ea2e08
joaovitor <joaovitor@joaovitors-Mac-Pro.local>
2021-12-11 15:46:38 -0300

    :wrench: use .env

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,13 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+        },
+      ],
+      [
         'module-resolver',
         {
           extensions: [
